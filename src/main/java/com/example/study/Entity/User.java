545995308d8bd4144081898bec59d00009829a05
Suanna01 <yangnony01@gmail.com>
2023-11-16 14:42:10 +0900

    1주차 : 회원가입, 로그인

---
+++ src/main/java/com/example/study/Entity/User.java
@@ -0,0 +1,23 @@
+package com.example.study.Entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(name = "username", nullable = false)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+}
