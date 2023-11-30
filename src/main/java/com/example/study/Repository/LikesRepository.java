289d5dcabcb0592b3500ac2fe51f6ca0a0e589e9
leejw-lu <jiwoojenny@naver.com>
2023-11-30 22:08:11 +0900

    3주차: 좋아요 repository에 Transactional 추가

--- src/main/java/com/example/study/Repository/LikesRepository.java
+++ src/main/java/com/example/study/Repository/LikesRepository.java
@@ -3,11 +3,13 @@ package com.example.study.Repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.study.Entity.Board;
 import com.example.study.Entity.Likes;
 import com.example.study.Entity.User;
 
+@Transactional
 public interface LikesRepository extends JpaRepository<Likes, Long> {
 	//좋아요 여부 확인
 	Optional<Object> findByUserAndBoard(User user, Board board);
